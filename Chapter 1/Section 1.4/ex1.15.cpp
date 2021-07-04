7e16042b0c6ef8d3ff4f87dd70cc85564bbfffb4
sanzi0 <85235054+sanzi0@users.noreply.github.com>
2021-07-04 15:21:57 -0400

    Add files via upload

---
+++ Chapter 1/Section 1.4/ex1.15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// error: missing ) in parameter list for main
+int main ( {
+	// error: used colon, not a semicolon, after endl
+	std::cout << "Read each file." << std::endl:
+	// error: missing quotes around string literal
+	std::cout << Update master. << std::endl;
+	// error: second output operator is missing
+	std::cout << "Write new master." std::endl;
+	// error: missing ; on return statement
+	return 0
+
+	int v1 = 0, v2 = 0;
+	std::cin >> v >> v2; // error: uses "v" not "v1"
+	// error: cout not defined; should be std::cout
+	cout << v1 + v2 << std::endl; return 0;
+}
