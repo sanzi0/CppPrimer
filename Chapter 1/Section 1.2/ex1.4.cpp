f7310e3ac2766120e2cefddc45e989e2c07d3caf
sanzi0 <85235054+sanzi0@users.noreply.github.com>
2021-07-04 10:41:40 -0400

    Add files via upload

---
+++ Chapter 1/Section 1.2/ex1.4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Enter two numbers:" << std::endl;
+	int a = 0, b = 0;
+	std::cin >> a >> b;
+	std::cout << "The product of " << a << " and " << b << " is "
+			  << a * b;
+	return 0;
+}
