07392954ab3f29c205003bc51bc8842a5dd1fbd5
sanzi0 <85235054+sanzi0@users.noreply.github.com>
2021-07-04 11:54:42 -0400

    Update ex1.11.cpp

--- Chapter 1/Section 1.4/ex1.11.cpp
+++ Chapter 1/Section 1.4/ex1.11.cpp
@@ -3,8 +3,8 @@
 int main() {
 	int a = 0, b = 0;
 	// The first number must be bigger than the second, or else
-	// the program will run forever. This is solved a couple
-	// exercises ahead.
+	// the program will run forever. We build something that uses
+	// whatever order a few exercises ahead.
 	std::cout << "Enter a number: ";
 	std::cin >> a;
 	std::cout << "Enter another number: ";
