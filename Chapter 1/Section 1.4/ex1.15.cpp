19cfb5033ea653cd748f632bf25c1da4cd4cff7c
sanzi0 <85235054+sanzi0@users.noreply.github.com>
2021-07-04 15:41:36 -0400

    Update ex1.15.cpp

--- Chapter 1/Section 1.4/ex1.15.cpp
+++ Chapter 1/Section 1.4/ex1.15.cpp
@@ -14,5 +14,6 @@ int main ( {
 	int v1 = 0, v2 = 0;
 	std::cin >> v >> v2; // error: uses "v" not "v1"
 	// error: cout not defined; should be std::cout
-	cout << v1 + v2 << std::endl; return 0;
+	cout << v1 + v2 << std::endl; 
+	return 0;
 }
