d3ddeecf9fd71de12ce1df079a5c6a3c4f887944
sanzi0 <85235054+sanzi0@users.noreply.github.com>
2021-07-05 13:23:02 -0400

    Add files via upload

---
+++ Chapter 2/Section 2.3/ex2.18.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int a = 512;
+	int b = 1024;
+
+	// initialize pt as a pointer
+	// to a
+	int *pt = &a;
+
+	// changes address stored in pt
+	// it now points to b
+	pt = &b;
+
+	// access object through dereference
+	// assigns 256 to b
+	*pt = 256;
+
+	return 0;
+}
